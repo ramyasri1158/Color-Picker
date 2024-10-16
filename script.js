7d13628bbcb866e995b2685fe90aa984c9661e2d
ramya <165128565+ramyasri1158@users.noreply.github.com>
2024-10-16 11:19:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let a=document.getElementById("colorpickercontainer");
+let b=document.getElementById("selectedcolorhexcode");
+
+function button1(){
+    a.style.background='#e0e0e0';
+    b.textContent="#e0e0e0";
+    
+
+}
+function button2(){
+    a.style.background='#6fcf97';
+    b.textContent="#6fcf97"
+
+}
+function button3(){
+    a.style.background='#56ccf2';
+    b.textContent="#56ccf2"
+
+}
+function button4(){
+    a.style.background='#bb6bd9';
+    b.textContent="#bb6bd9"
+
+}
